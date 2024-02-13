22497286c2a442314961373907ebca9bc3164c59
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-02-13 07:39:56 -0300

    init

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express'); //requiring express module
 
 const app = express(); //app variable for express
@@ -7,16 +8,30 @@ const app = express(); //app variable for express
 // req = request
 // res = response
 // '/' = url
-app.get('/', (req, res) => {
-  // res.status(200).send('Hello from the server side!'); //send response for the client side with 200=success
-  //sending response for client side
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//   // res.status(200).send('Hello from the server side!'); //send response for the client side with 200=success
+//   //sending response for client side
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side', app: 'Natours' });
+// });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...'); //send response for the client side
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...'); //send response for the client side
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 //start server
