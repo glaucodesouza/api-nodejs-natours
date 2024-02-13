ffa8e8002ecd9da23fb922c0f79dcd593d3ac1e2
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-02-13 17:19:05 -0300

    refactoring routes

--- app.js
+++ app.js
@@ -2,31 +2,18 @@ const fs = require('fs');
 const express = require('express'); //requiring express module
 
 const app = express(); //app variable for express
-
 app.use(express.json()); //middleware that executes in the middle of processing a request
 
-// get request from browser
-// get = http method
-// req = request
-// res = response
-// '/' = url
-// app.get('/', (req, res) => {
-//   // res.status(200).send('Hello from the server side!'); //send response for the client side with 200=success
-//   //sending response for client side
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint...'); //send response for the client side
-// });
-
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+  fs.readFileSync(
+    `${__dirname}/dev-data/data/tours-simple.json`
+  )
 );
 
-app.get('/api/v1/tours', (req, res) => {
+//----------------------------------------------------------------
+// GET Tours (Get all tours)
+//----------------------------------------------------------------
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -34,12 +21,12 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
-});
+};
 
 //----------------------------------------------------------------
-// GET /api/v1/tours
+// GET Tour (Get tour)
 //----------------------------------------------------------------
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1; //=====>trick in Javascript to format string to integer
   const tour = tours.find((el) => el.id === id);
@@ -58,12 +45,11 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour,
     },
   });
-});
-
+};
 //----------------------------------------------------------------
-// POST /api/v1/tours
+// POST for /api/v1/tours (Create Tour)
 //----------------------------------------------------------------
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
@@ -82,12 +68,12 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
 
 //----------------------------------------------------------------
-// PATCH
+// PATCH (Update Tour)
 //----------------------------------------------------------------
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     //multiply by 1, to turn it Integer
     return res.status(404).json({
@@ -102,12 +88,11 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tour: '<Updated tour here>',
     },
   });
-});
-
+};
 //----------------------------------------------------------------
-// DELETE
+// DELETE Tour
 //----------------------------------------------------------------
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
   //multiply by 1, to turn it Integer
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
@@ -121,9 +106,28 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: null,
   });
-});
+};
 
-//start server
+// app.get("/api/v1/tours", getAllTours);
+// app.get("/api/v1/tours/:id", getTour);
+// app.post("/api/v1/tours", createTour);
+// app.patch("/api/v1/tours/:id", updateTour);
+// app.delete("/api/v1/tours/:id", deleteTour);
+
+app
+  .route('/api/v1/tours')
+  .get(getAllTours)
+  .post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+//----------------------------------------------------------------
+//Start Server
+//----------------------------------------------------------------
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
