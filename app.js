eb32ba7855b442bdc4171a195d8615ea84be48bf
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-02-13 08:39:23 -0300

    post method

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express'); //requiring express module
 
 const app = express(); //app variable for express
 
+app.use(express.json()); //middleware that executes in the middle of processing a request
+
 // get request from browser
 // get = http method
 // req = request
@@ -32,6 +34,27 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 //start server
