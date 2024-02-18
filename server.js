39f4630a97d23afc70821113bca431346440b99d
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-02-18 19:40:59 -0300

    Mongoose DATABASE connection to Atlas Cloud

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const DB = process.env.DATABASE2.replace(
   process.env.DATABASE_PASSWORD
 );
 
+//----------------------------------------------------------------
 // DATABASE connection
+// mongoose is an higher layer abstraction for mongoDB
+//----------------------------------------------------------------
 mongoose
   // local database version
   // .connect(process.env.DATABASE_LOCAL, {
@@ -25,7 +28,43 @@ mongoose
   .then(() =>
     console.log('DB connection successful!')
   )
-  .catch(err => console.error(err));
+  .catch(err => console.log(err));
+
+//----------------------------------------------------------------
+// mongoose Schema
+//----------------------------------------------------------------
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+    required: [
+      true,
+      'A tour must have a rating'
+    ]
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  rating: 4.7,
+  price: 997
+});
+
+testTour
+  .save() //Promise to save document to the DB
+  .then(doc => console.log(doc))
+  .catch(err => console.log('ERROR 💥', err));
 
 // console.log(app.get('env')); //write environment variables on terminal
 // console.log(process.env); // write environment variables on terminal
