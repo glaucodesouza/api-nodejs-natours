c6c0d4fca9e0e329ac4206bfdeac5b7af25942b4
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-02-18 18:16:51 -0300

    Install Mongoose and DB Connection to Atlas Cloud

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE2.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// DATABASE connection
+mongoose
+  // local database version
+  // .connect(process.env.DATABASE_LOCAL, {
+  // hosted database Atlas version
+  .connect(DB, {
+    // just some options to deal with deprecation warnings.
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() =>
+    console.log('DB connection successful!')
+  )
+  .catch(err => console.error(err));
 
 // console.log(app.get('env')); //write environment variables on terminal
 // console.log(process.env); // write environment variables on terminal
