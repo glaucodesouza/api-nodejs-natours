3ac68eab1fa1fdc13110940a519170da4de10c29
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-02-18 00:56:49 -0300

    Environment Variables

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ const app = express(); //app variable for express
 // 1) MIDDLEWARERS
 // middleware that executes in the middle of processing a request
 //----------------------------------------------------------------
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-const port = 3000;
+// console.log(app.get('env')); //write environment variables on terminal
+// console.log(process.env); // write environment variables on terminal
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
