6a096e65f1576fa11d37c01d15c036fd3270afbe
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-03-16 16:45:37 -0300

    Document middleware (pre, post)

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -11,6 +12,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true
     },
+    slug: String,
     duration: {
       type: Number,
       required: [
@@ -92,6 +94,24 @@ tourSchema
   .get(function() {
     return this.duration / 7;
   });
+
+// Document Middleware: runs before .save() and .create()
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, {
+    lower: true
+  });
+  next();
+});
+
+// tourSchema.pre('save', function(next) {
+//   console.log('We will save document...');
+//   next();
+// });
+
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
