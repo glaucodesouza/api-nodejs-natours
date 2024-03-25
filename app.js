9e92b1b96d234485f0e0d6a3293a3195f0db79e6
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-03-24 22:02:54 -0300

    Handle all routes

--- app.js
+++ app.js
@@ -18,9 +18,10 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log(`Hello from the middleware 👋🏻`);
+  console.log('Hello from the middleware 👋🏻');
   next();
 });
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -32,5 +33,19 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+//Handle all routes and urls (get, post, put, delete, etc)
+//all=all the verbs create, post, deletc, etc
+//'*' means all the routes /api/v1/tours, etc.
+//IMPORTANT:
+//Don't put it in the start of the file...
+//IT WILL ONLY give this error, because executions reached this function here.
+//SO it is because it did not find another correct route.
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`
+  });
+});
 
 module.exports = app;
